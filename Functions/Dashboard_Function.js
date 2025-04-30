a4d22af2bfc7ab04e847b9dc7ae52a593825dd79
Engr. Nathan <sfnathanvillamor@gmail.com>
2025-04-30 21:18:31 +0800

    feat - added functionality to cart button

---
+++ Functions/Dashboard_Function.js
@@ -0,0 +1,7 @@
+document.getElementById("navCartBtn").onclick = function() {
+    document.getElementById("cartContainer").style.right = "0";
+}
+
+document.getElementById("cartExit").onclick = function() {
+    document.getElementById("cartContainer").style.right = "-35rem";
+}
