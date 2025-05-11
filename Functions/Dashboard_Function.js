3e49cc0f998aef0ea49782b13a1a053c2fbdbcbd
Engr. Nathan <sfnathanvillamor@gmail.com>
2025-05-12 01:36:41 +0800

    all

--- Functions/Dashboard_Function.js
+++ Functions/Dashboard_Function.js
@@ -1,3 +1,4 @@
+// BUTTON FUNCTIONALITIES
 document.getElementById("navCartBtn").onclick = function() {
     document.getElementById("cartContainer").style.right = "0";
 }
@@ -20,4 +21,33 @@ document.getElementById("navReceiptBtn").onclick = function() {
 
 document.getElementById("receiptExit").onclick = function() {
     document.getElementById("receiptContainer").style.right = "-35rem";
+}
+
+
+
+
+
+
+
+
+
+
+// SEARCH BAR FUNCTIONALITIES 
+
+
+
+
+
+
+
+
+
+
+// TO CART FUNCTIONALITY
+let itemPriceContainer = document.querySelectorAll(".item-price");
+let calorieContainer = document.querySelectorAll(".food-calorie");
+let proteinContainer = document.querySelectorAll(".food-protein");
+
+function addToCart () {
+
 }
---
+++ Functions/Login_Functions.js
@@ -0,0 +1,11 @@
+document.getElementById("toSignupForm").onclick = function () {
+    document.getElementById("form-block").style.left = "50%";
+    document.querySelector(".to-login").style.display = "none";
+    document.querySelector(".to-signup").style.display = "flex";
+}
+
+document.getElementById("toLoginForm").onclick = function () {
+    document.getElementById("form-block").style.left = 0;
+    document.querySelector(".to-login").style.display = "flex";
+    document.querySelector(".to-signup").style.display = "none";
+}
